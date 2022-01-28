fc6f72f4f06f8732f34b345d841c897a372447bd
nirzaf <mfmfazrin1986@gmail.com>
2022-01-28 20:37:32 +0530

    //objects         var c1 = new Customer();         var c2 = new Customer();

--- ClassExample/Program.cs
+++ ClassExample/Program.cs
@@ -3,12 +3,9 @@ public class Sample
 {
     static void Main()
     {
-        //reference variables
-        Customer c1, c2;
-
         //objects
-        c1 = new Customer();
-        c2 = new Customer();
+        var c1 = new Customer();
+        var c2 = new Customer();
 
         c1.custmerID = 123;
         c1.customerName = "Farook";
