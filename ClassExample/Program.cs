fa7e2f47f36b3d616afffd57105665860ca8a17b
nirzaf <mfmfazrin1986@gmail.com>
2022-01-28 20:37:05 +0530

    WriteLine("Customer Id : {0}, Customer Name:{1}", c1.custmerID, c1.customerName);         WriteLine("Customer Id : {0}, Customer Name:{1}", c2.custmerID, c2.customerName);

--- ClassExample/Program.cs
+++ ClassExample/Program.cs
@@ -1,4 +1,5 @@
-﻿public class Sample
+﻿using static System.Console;
+public class Sample
 {
     static void Main()
     {
@@ -8,6 +9,18 @@
         //objects
         c1 = new Customer();
         c2 = new Customer();
+
+        c1.custmerID = 123;
+        c1.customerName = "Farook";
+        c2.custmerID = 21112;
+        c2.customerName = "Fazrin";
+
+
+        WriteLine("Customer Id : {0}, Customer Name:{1}", c1.custmerID, c1.customerName);
+        WriteLine("Customer Id : {0}, Customer Name:{1}", c2.custmerID, c2.customerName);
+
+        ReadKey();
+
     }
 }
 
